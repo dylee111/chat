af06f7cb6ca865bb7a572492d24bb0c7ab5076f7
DDDZZZ <dylee0568@gmail.com>
2022-02-26 20:56:29 +0900

    Initial commit

---
+++ src/test/java/chattingdemo/chat/ChattingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package chattingdemo.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
